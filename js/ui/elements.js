c893a9fa55f7efa21968646ba7879ea8f2820a6c
Yibo_Mero <xuyibo2002@outlook.com>
2025-11-20 16:46:53 +0800

    优化文件结构

---
+++ js/ui/elements.js
@@ -0,0 +1,13 @@
+// js/ui/elements.js
+
+export const elements = {
+    baudRate: document.getElementById("baud-rate"),
+    canMessagesTable: document.getElementById("can-messages-table"),
+    addRowButton: document.getElementById("add-row-button"),
+    minLoadResult: document.getElementById("min-load-result"),
+    avgLoadResult: document.getElementById("avg-load-result"),
+    maxLoadResult: document.getElementById("max-load-result"),
+    loadBar: document.getElementById("load-bar"),
+    resetButton: document.getElementById("reset-button"),
+    visualizationContainer: document.getElementById("visualization-container")
+};
